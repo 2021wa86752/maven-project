db73466d7077adb24f0a62371e5a2e10316294d2
2021wa86752 <2021wa86752@wilp.bits-pilani.ac.in>
2025-07-14 16:22:08 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
 package com.bits_pilani;
 
 /**
- * Hello world!
+ * Hello Bits Pilani!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Bits Pilani!" );
     }
 }
